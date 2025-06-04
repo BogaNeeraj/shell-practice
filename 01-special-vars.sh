888bd9adbd0c66ab36d8c071667271e4d1d7224a
Neeraj <neerajchintwo@gmail.com>
2025-06-04 20:26:40 +0530

    helloworld

--- 01-special-vars.sh
+++ 01-special-vars.sh
@@ -2,5 +2,8 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
+echo "script name : $0"
+echo "present working directort :$pwd"
+echo "home directory of the user :"$Home"
 
 
