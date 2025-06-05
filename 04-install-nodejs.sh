758282f29dc6f700dc554e9a134ed287130fd535
Neeraj <neerajchintwo@gmail.com>
2025-06-05 19:03:03 +0530

    conditions

--- 04-install-nodejs.sh
+++ 04-install-nodejs.sh
@@ -10,4 +10,12 @@ else
  echo " you are running with root user"
 fi
 
-dnf install nodejs:20 -y
+dnf install node1js:20 -y
+
+if [ $? -ne 0 ]
+then 
+ echo "installing nodejs is successfull"
+else 
+ echo "installing nodejs is ..Failure"
+ exit 1
+fi
