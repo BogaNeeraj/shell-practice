6cb90244d386957824e6c674c1c8203cd2e7cf3a
Neeraj <neerajchintwo@gmail.com>
2025-06-05 23:49:56 +0530

    conditions

--- 07-loops.sh
+++ 07-loops.sh
@@ -9,9 +9,9 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/logs/shellscript1.logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE:"$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 TIMESTAMP=$(date +%F-%M-%D-%S)
-PACKAGES= ("mysql", "nginx")
+PACKAGES=("mysql", "nginx")
 
 mkdir -p $LOGS_FOLDER
 echo "script execution started at $TIMESTAMP" &>> $LOG_FILE
