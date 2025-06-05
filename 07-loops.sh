2c370b11fae86d261ae56361317a1c195f6a3188
Neeraj <neerajchintwo@gmail.com>
2025-06-05 23:46:51 +0530

    conditions

---
+++ 07-loops.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/logs/shellscript1.logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE:"$LOGS_FOLDER/$SCRIPT_NAME.log"
+TIMESTAMP=$(date +%F-%M-%D-%S)
+PACKAGES= ("mysql", "nginx")
+
+mkdir -p $LOGS_FOLDER
+echo "script execution started at $TIMESTAMP" &>> $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then 
+ echo "$R Error::you are not running with the root user $N" >> $LOG_FILE
+else
+ echo "$G you are running with the root user $N" >> $LOG_FILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+     echo " $2 is successfull "
+    else 
+    echo " $2 is Failure"
+    exit 1
+    fi
+
+}
+
+for packages in ${PACKAGES[@]}
+do
+ dnf list installed $package &>> $LOG_FILE
+ if [ $? -ne 0 ]
+ then 
+  echo "package is going to installed "
+  dnf install $package -y &>> $LOG_FILE
+  VALIDATE $? "$package"
+else 
+ echo "package is already installed"
+fi
+done
+
+
+
+
+
