996892950e2da16e012c3f252c35c97127609408
Neeraj <neerajchintwo@gmail.com>
2025-06-05 19:45:53 +0530

    conditions

---
+++ 05-Functions.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+  echo "error:: please run as root user"
+  exit 1 # You can give other than 0
+else 
+  echo "you are running with the root user"
+fi
+
+VALIDATION() {
+    if [ $1 -eq 0 ]
+    then 
+      echo "$2...Sucessful"    
+    else
+      echo "$2...failed"
+      exit 1
+    fi
+}
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+ echo " my sql is not installed, it is going to install"
+ dnf install mysql -y
+ VALIDATION $? "MYSQL"
+fi
+else
+ echo "my sql is installed"
+
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then 
+ echo " my python3 is not installed, it is going to install"
+ dnf install python3 -y
+VALIDATION $? "python3"
+fi
+else
+ echo "python3 is installed"
+
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+ echo " nginx is not installed, it is going to install"
+ dnf install nginx -y
+ VALIDATION $? "nginx"
+fi
+else
+ echo "nginx is installed"
+
+fi
