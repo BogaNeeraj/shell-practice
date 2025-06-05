0b03e323de368bb1fad4a1698f54c98328c308f8
Neeraj <neerajchintwo@gmail.com>
2025-06-05 17:59:16 +0530

    helloworld

---
+++ 02-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+number=$!
+
+if [ number -lt 10 ]
+then 
+  echo "the given number $number is less than 10"
+else
+  echo "the given number $number is not less than 10"
+
+
+# -gt -- greater than
+# -lt --> less than
+# -eq --> equals to
+# -ne --> not equals
+
