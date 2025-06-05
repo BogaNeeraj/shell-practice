0d35d255b79c457891ea688f6c033e022ef02a82
Neeraj <neerajchintwo@gmail.com>
2025-06-05 18:36:28 +0530

    helloworld

--- 03-install-mysql.sh
+++ 03-install-mysql.sh
@@ -2,10 +2,10 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -eq 0 ]
 then
   echo "error:: please run as root user"
-  exit 1 # You can give other than 0
+ # exit 1 # You can give other than 0
 else 
   echo "you are running with the root user"
 fi
